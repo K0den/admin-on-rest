15bbdc0669fd6a74ff445491dcafa2e7e67ae4a4
Michael Nielsen <mcl@codewizards.dk>
2017-07-25 23:08:08 +0200

    Added delete handling

--- src/reducer/resource/data.js
+++ src/reducer/resource/data.js
@@ -1,12 +1,14 @@
-import { FETCH_END } from '../../actions/fetchActions';
+import {FETCH_END} from "../../actions/fetchActions";
 import {
-    GET_LIST,
-    GET_ONE,
-    GET_MANY,
-    GET_MANY_REFERENCE,
-    CREATE,
-    UPDATE,
-} from '../../rest/types';
+  GET_LIST,
+  GET_ONE,
+  GET_MANY,
+  GET_MANY_REFERENCE,
+  CREATE,
+  UPDATE,
+  DELETE,
+} from "../../rest/types";
+import _ from "lodash";
 
 /**
  * The data state is an instance pool, which keeps track of the fetch date of each instance.
@@ -22,7 +24,7 @@ import {
  * }
  */
 
-const cacheDuration = 10 * 60 * 1000; // ten minutes
+const cacheDuration = 60 * 60 * 1000 * 24 * 365; // One year
 
 /**
  * Add new records to the pool, and remove outdated ones.
@@ -32,65 +34,72 @@ const cacheDuration = 10 * 60 * 1000; // ten minutes
  * only once fresh data is fetched.
  */
 const addRecords = (newRecords = [], oldRecords) => {
-    // prepare new records and timestamp them
-    const newRecordsById = newRecords.reduce((prev, record) => {
-        prev[record.id] = record; // eslint-disable-line no-param-reassign
-        return prev;
-    }, {});
-    const now = new Date();
-    const newRecordsFetchedAt = newRecords.reduce((prev, record) => {
-        prev[record.id] = now; // eslint-disable-line no-param-reassign
-        return prev;
-    }, {});
-    // remove outdated old records
-    const latestValidDate = new Date();
-    latestValidDate.setTime(latestValidDate.getTime() - cacheDuration);
-    const oldValidRecordIds = oldRecords.fetchedAt
-        ? Object.keys(oldRecords.fetchedAt)
-            .filter(id => oldRecords.fetchedAt[id] > latestValidDate)
-        : [];
-    const oldValidRecords = oldValidRecordIds.reduce((prev, id) => {
-        prev[id] = oldRecords[id]; // eslint-disable-line no-param-reassign
-        return prev;
-    }, {});
-    const oldValidRecordsFetchedAt = oldValidRecordIds.reduce((prev, id) => {
-        prev[id] = oldRecords.fetchedAt[id]; // eslint-disable-line no-param-reassign
-        return prev;
-    }, {});
-    // combine old records and new records
-    const records = {
-        ...oldValidRecords,
-        ...newRecordsById,
-    };
-    Object.defineProperty(records, 'fetchedAt', { value: {
-        ...oldValidRecordsFetchedAt,
-        ...newRecordsFetchedAt,
-    } }); // non enumerable by default
-    return records;
+  // prepare new records and timestamp them
+  const newRecordsById = newRecords.reduce((prev, record) => {
+    prev[record.id] = record; // eslint-disable-line no-param-reassign
+    return prev;
+  }, {});
+  const now = new Date();
+  const newRecordsFetchedAt = newRecords.reduce((prev, record) => {
+    prev[record.id] = now; // eslint-disable-line no-param-reassign
+    return prev;
+  }, {});
+  // remove outdated old records
+  const latestValidDate = new Date();
+  latestValidDate.setTime(latestValidDate.getTime() - cacheDuration);
+  const oldValidRecordIds = oldRecords.fetchedAt
+    ? Object.keys(oldRecords.fetchedAt).filter(id => oldRecords.fetchedAt[id] > latestValidDate)
+    : [];
+  const oldValidRecords = oldValidRecordIds.reduce((prev, id) => {
+    prev[id] = oldRecords[id]; // eslint-disable-line no-param-reassign
+    return prev;
+  }, {});
+  const oldValidRecordsFetchedAt = oldValidRecordIds.reduce((prev, id) => {
+    prev[id] = oldRecords.fetchedAt[id]; // eslint-disable-line no-param-reassign
+    return prev;
+  }, {});
+  // combine old records and new records
+  const records = {
+    ...oldValidRecords,
+    ...newRecordsById,
+  };
+  Object.defineProperty(records, "fetchedAt", {
+    value: {
+      ...oldValidRecordsFetchedAt,
+      ...newRecordsFetchedAt,
+    },
+  }); // non enumerable by default
+  return records;
+};
+
+const removeRecord = (params, oldRecords) => {
+  return _.omit(oldRecords, params.id);
 };
 
 const initialState = {};
-Object.defineProperty(initialState, 'fetchedAt', { value: {} }); // non enumerable by default
+Object.defineProperty(initialState, "fetchedAt", {value: {}}); // non enumerable by default
 
-export default resource => (previousState = initialState, { payload, meta }) => {
-    if (!meta || meta.resource !== resource) {
-        return previousState;
-    }
-    if (!meta.fetchResponse || meta.fetchStatus !== FETCH_END) {
-        return previousState;
-    }
-    switch (meta.fetchResponse) {
+export default resource => (previousState = initialState, {payload, meta}) => {
+  if (!meta || meta.resource !== resource) {
+    return previousState;
+  }
+  if (!meta.fetchResponse || meta.fetchStatus !== FETCH_END) {
+    return previousState;
+  }
+  switch (meta.fetchResponse) {
     case GET_LIST:
     case GET_MANY:
     case GET_MANY_REFERENCE:
-        return addRecords(payload.data, previousState);
+      return addRecords(payload.data, previousState);
     case GET_ONE:
     case UPDATE:
     case CREATE:
-        return addRecords([payload.data], previousState);
+      return addRecords([payload.data], previousState);
+    case DELETE:
+      return removeRecord(payload.data, previousState);
     default:
-        return previousState;
-    }
+      return previousState;
+  }
 };
 
 export const getRecord = (state, id) => state[id];
