cc5b51670284185f07f523bfe6f242937af51683
Michael Nielsen <mcl@codewizards.dk>
2017-10-04 20:05:24 +0200

    changed stuff

--- src/mui/layout/MenuItemLink.js
+++ src/mui/layout/MenuItemLink.js
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 
-import MenuItem from 'material-ui/MenuItem';
-import { withRouter } from 'react-router';
+import MenuItem from "material-ui/MenuItem";
+import {withRouter} from "react-router";
 
 export class MenuItemLinkComponent extends Component {
-    static propTypes = {
-        history: PropTypes.object.isRequired,
-        onTouchTap: PropTypes.func.isRequired,
-        to: PropTypes.string.isRequired,
-    }
+  static propTypes = {
+    history: PropTypes.object.isRequired,
+    to: PropTypes.string.isRequired,
+    onTouchTap: PropTypes.func,
+  };
 
-    handleMenuTap = () => {
-        this.props.history.push(this.props.to);
-        this.props.onTouchTap();
-    }
-    render() {
-        const { history, match, location, staticContext, ...props } = this.props; // eslint-disable-line
+  handleMenuTap = () => {
+    const {history, onTouchTap} = this.props;
+    history.push(this.props.to);
+    if (onTouchTap) onTouchTap();
+  };
+  render() {
+    const {history, match, location, staticContext, ...props} = this.props; // eslint-disable-line
 
-        return (
-            <MenuItem
-                {...props}
-                onTouchTap={this.handleMenuTap}
-            />
-        );
-    }
+    return <MenuItem {...props} onTouchTap={this.handleMenuTap} />;
+  }
 }
 
 export default withRouter(MenuItemLinkComponent);
